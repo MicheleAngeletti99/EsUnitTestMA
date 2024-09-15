517afdf91764a9804e44f1378bfdc7d1500408d6
Michele Angeletti <m.angeletti99@gmail.com>
2024-09-15 19:32:55 +0200

    Esercizio

---
+++ src/main/java/com/example/esunittest/EsUnitTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.esunittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsUnitTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsUnitTestApplication.class, args);
+	}
+
+}
